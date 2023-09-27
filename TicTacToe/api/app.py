6acd99bb3a2570e60e0ef03b870d786a8646ccd5
Judy Chen <jc942@duke.edu>
2023-09-27 19:07:28 -0400

    set up /move route for the robot to get the URL parameters row and col

--- TicTacToe/api/app.py
+++ TicTacToe/api/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+# for the robot
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# GET endpoint for robot to get where it should move to
+@app.route("/move")
+def move():
+    row = request.args.get('row')
+    col = request.args.get('col')
+    return '''<h1>The given position is: {}, {}</h1>'''.format(row, col)
