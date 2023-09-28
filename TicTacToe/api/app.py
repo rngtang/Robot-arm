356fc515489d156caa76349258e1a46fa26739fc
Judy Chen <jc942@duke.edu>
2023-09-28 13:01:49 -0400

    finally fixed url

--- TicTacToe/api/app.py
+++ TicTacToe/api/app.py
@@ -2,23 +2,20 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',port=5000,debug=True)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-# GET endpoint for robot to get where it should move to
 # position is two numbers (row, col) put together
-@app.route('/move/<position>', methods=['GET'])
-@app.route('/move/', defaults={'position' : '99'})
-def move(position):
-    response = request.get(url="http://0.0.0.0:5000/send/")
-        # row = request.get(position[0])
-        # col = request.get(position[1])
+@app.route("/move")
+def access_position():
+    position = request.args.get('pos')
+    result = robot_move(position)
+    return result
 
-    row = position[0]
-    col = position[1]
-    return '''<h1>The given position is: {}, {}</h1>'''.format(row, col)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+def robot_move(position):
+    return "hi zhichen"
+    # return '''<h1>The given position is: {}, {}</h1>'''.format(row, col)
