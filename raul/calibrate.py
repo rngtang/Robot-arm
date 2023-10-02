319d14837a3f977405b85729726644e9ab511af7
Raul <ren11@duke.edu>
2023-10-02 14:14:21 -0400

    calibrate

--- raul/calibrate.py
+++ raul/calibrate.py
@@ -1,10 +1,14 @@
 from pymycobot.mycobot import MyCobot
+import time
 
 mc = MyCobot("/dev/ttyAMA0", 1000000)
 
-mc.send_angles([-50, 2, 0, 0, -8, 1], 70)
+# mc.send_angles([-50, 2, 0, 0, -8, 1], 70)
+mc.send_angles([0,0,0,0,0,0], 70)
 
-for i in range(1, 7):
-    mc.set_servo_calibration(i)
+# for i in range(1, 7):
+#     mc.set_servo_calibration(i)
+
+time.sleep(3)
 
 mc.release_all_servos()
