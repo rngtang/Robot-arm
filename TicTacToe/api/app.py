330d70b76f9a2eee9c5ab5587566a479cdff149a
Raul <ren11@duke.edu>
2023-10-03 11:15:59 -0400

    test

--- TicTacToe/api/app.py
+++ TicTacToe/api/app.py
@@ -49,14 +49,3 @@ def robot_move(coords):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001,debug=True)
-
-    # Idk why is this here
-    # # Testing movements
-    # if(coords[0] == 1):
-    #     mc.sync_send_angles([-40, -135, 90, -50, 0, 0], 60, 3)
-    # elif(coords[0] == 2):
-    #     mc.sync_send_angles([-50, -135, 90, -50, 0, 0], 60, 3)
-    # time.sleep(1)
-    # mc.send_angles([-50, 0, 0, 0, 0, 0], 70)
-    # # Prints the coordinates to move
-    # # return '''<h1>The given position is: {}, {}</h1>'''.format(coords[0], coords[1])
--- TicTacToe/ttt.py
+++ TicTacToe/ttt.py
@@ -144,7 +144,5 @@ def findBestMove(board) :
 # Finds the best move and sends the POST request to the robot
 def updateBoard(board): 
 	move = findBestMove(board)
-	# URL + sends request
-	# url = "http://10.197.42.231:5001/move?pos={pos}".format(pos = str(move[0])+str(move[1]))
 	url = "http://10.194.72.227:5000/move?pos={pos}".format(pos = str(move[0])+str(move[1]))
 	respose  = requests.get(url)
