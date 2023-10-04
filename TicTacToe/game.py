23f0803a76131c36f0dccda609e193962b07d926
Johnny <jac188@duke.edu>
2023-10-04 17:42:11 -0400

    edits

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -396,7 +396,7 @@ def buttonclick(x):
         # tkinter.messagebox.showinfo("Tic Tac Toe","Match is Draw.")
 
     elif runScript == 1: #run the script to call robot move if the player made their move and game is not over
-        window.update()
+        # window.update()
         updateBoard(currGame)
         print("test")
     
