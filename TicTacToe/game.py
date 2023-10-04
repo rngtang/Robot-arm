5f27510a8b7ab206f324f3bd36d0458822ad12ad
Johnny <jac188@duke.edu>
2023-10-04 18:46:20 -0400

    screen enlarging

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -70,7 +70,7 @@ print("Screen width:", screen_width)
 print("Screen height:", screen_height)
 
 frame1=tk.Frame(master=window,borderwidth=2,relief=tk.SUNKEN,bg='#00539B')
-frame1.pack(padx=0,pady=0)
+frame1.pack(padx=10,pady=10)
 
 # Creates the buttons grid
 button1=tk.Button(master=frame1,text='',width=14,height=5,bg='white',command=lambda : buttonclick(1))
