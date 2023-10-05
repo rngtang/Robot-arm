50dda7343e3bce794f6ee9376322bce8a97ae581
Raul <ren11@duke.edu>
2023-10-05 16:22:03 -0400

    adjustments

--- TicTacToe/api/app.py
+++ TicTacToe/api/app.py
@@ -30,15 +30,15 @@ TIC_TAC_TOE_COORDS = {
 }
 
 TIC_TAC_TOE_ANGLES = {
-    "00": [248, 30, -12, -180, 0, -68],
-    "10": [235, 28, -13, -180, 0, -67],
-    "20": [221, 30, -13, -180, -1, -65],
-    "01": [248, 4, -13, -180, 0, -75],
-    "11": [235, 2, -13, -180, 0, -74],
-    "21": [222, -2, -12, -180, -2, -73],
-    "02": [249, -23, -11, -177, -2, -80],
-    "12": [235, -23, -12, -176, -1, -80],
-    "22": [220, -23, -14, -179, -2, -79]
+    "00": [21.7, -100.54, -43.68, 55.63, -1.13, 0],
+    "10": [22.5, -95.62, -56.59, 61.52, -1.04, -2.98],
+    "20": [24.34, -92.45, -70.04, 71.88, -1.23, -5.17],
+    "01": [15.72, -100.54, -43.68, 55.63, -1.13, 0],
+    "11": [15.89, -96.84, -56.41, 64.02, -1.3, -6.15],
+    "21": [16.25, -92.45, -70.30, 72.41, -1.30, -5.17],
+    "02": [9.65, -100.54, -43.68, 55.63, -1.13, 6.94],
+    "12": [9.75, -96.84, -56.59, -65.2, 0.25, -6.05],
+    "22": [10.72, -92.45, -70.30, 72.5, -1.49, -5.53]
 }
 
 # Default route
@@ -75,7 +75,8 @@ def robot_move(coords):
         return "<h1>This is not a valid coordinate, please try again</h1>"
 
     try:
-        controls.send_coords(TIC_TAC_TOE_COORDS[coords], 70, 2)
+        controls.send_angles(TIC_TAC_TOE_ANGLES[coords], 70)
+        # controls.send_coords(TIC_TAC_TOE_COORDS[coords], 70, 2)
         time.sleep(2)
         controls.send_angles([0, 0, 0, 0, 0, 0], 70)
         time.sleep(1)
@@ -90,7 +91,7 @@ def test_all():
     for i in range(0, 3):
         for j in range(0,3):
             coords = str(i)+str(j)
-            controls.send_coords(TIC_TAC_TOE_COORDS[coords] + [180, 0, 0], 70, 2)
+            controls.send_angles(TIC_TAC_TOE_ANGLES[coords], 70)
             time.sleep(2)
             controls.send_angles([0, 0, 0, 0, 0, 0], 70)
             time.sleep(2)
