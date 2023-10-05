4dd041ee4497fb693d84139170d78a3b49d8a384
wuchangji <changji.wu@elephantrobotics.com>
2023-10-06 00:41:39 +0800

    Hard coded robot coordinates and removed .pyc files

--- TicTacToe/api/app.py
+++ TicTacToe/api/app.py
@@ -1,9 +1,5 @@
-# for the robot
 from flask import Flask, request
-# import requests
 import time
-from pymycobot.mycobot import MyCobot
-# mc = MyCobot("/dev/ttyAMA0", 1000000)
 
 app = Flask(__name__)
 
@@ -13,6 +9,18 @@ sys.path.append('/home/ubuntu/catkin_ws/src/mycobot_ros/mycobot_280/mycobot_280/
 from controls import Controls
 
 controls = Controls()
+
+TIC_TAC_TOE_COORDS = {
+    "00": [248, 30, -12],
+    "10": [235, 28, -13],
+    "20": [221, 30, -13],
+    "01": [248, 4, -13],
+    "11": [235, 2, -13],
+    "21": [222, -2, -12],
+    "02": [249, -23, -11],
+    "12": [235, -23, -12],
+    "22": [220, -23, -14]
+}
 
 # Default route
 @app.route("/")
@@ -27,45 +35,17 @@ def access_position():
 
     # Converts string into a list
     coords = [int(position[0]), int(position[1])]
-    print(coords)
     result = robot_move(coords)
     return result
 
 def robot_move(coords):
+    if coords not in TIC_TAC_TOE_COORDS:
+        return "<h1>This is not a valid coordinate, please try again</h1>"
 
-    # Johnny's
-    # Sends the position coordinates to the Flask API
-    # url = "http://localhost:5000/move"
-    # params = {"pos": str(coords[0]) + str(coords[1])}
-    # response = requests.get(url, params=params)
+    controls.send_coords(TIC_TAC_TOE_COORDS[coords].extend([180, 0, 0]), 70, 2)
+    time.sleep(2)
+    controls.send_angles([0, 0, 0, 0, 0, 0], 70)
 
-    # Returns the response from the Flask API
-    # return response.text
-
-    # Raul's
-    # Testing movements
-    # if(coords[0] == 1):
-    #     mc.sync_send_angles([0, -135, 90, -50, 0, 0], 60, 3)
-    # elif(coords[0] == 2):
-    #     mc.sync_send_angles([0, -135, 90, -50, 0, 0], 60, 3)
-    # time.sleep(3)
-    # mc.send_angles([0, 0, 0, 0, 0, 0], 70)
-    # Prints the coordinates to move
-
-    if coords[0] == coords[1] and coords[0] == 0:
-        controls.send_coords([249, 32, -12, 180, 1, -48], 70, 2)
-        time.sleep(2)
-        controls.send_angles([0, 0, 0, 0, 0, 0], 70)
-
-    elif coords[0] == coords[1] and coords[0] == 1:
-        controls.send_coords([239, 4, -12, -177, 1, -54], 70, 2)
-        time.sleep(2)
-        controls.send_angles([0, 0, 0, 0, 0, 0], 70)
-
-    else:
-        controls.send_coords([221, -23, -12, -180, 0, -59], 70, 2)
-        time.sleep(2)
-        controls.send_angles([0, 0, 0, 0, 0, 0], 70)
     return '''<h1>The given position is: {}, {}</h1>'''.format(coords[0], coords[1])
 
 if __name__ == '__main__':
