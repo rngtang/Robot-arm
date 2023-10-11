f9e928626a41e5e00d7c7d8b0158d42e2457c9df
Johnny <jac188@duke.edu>
2023-10-11 18:44:11 -0400

    testing

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -21,7 +21,7 @@ print("Screen width:", screen_width)
 print("Screen height:", screen_height)
 
 frame1=tk.Frame(master=window,borderwidth=2,relief=tk.SUNKEN)
-frame1.pack(padx=3,pady=2)
+frame1.pack(padx=0,pady=0)
 
 # Creates the buttons grid
 button1=tk.Button(master=frame1,text='',width=16,height=4,bg='white',activebackground='white',command=lambda : buttonclick(1), highlightthickness=2, highlightbackground='black')
