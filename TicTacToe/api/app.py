9ce5e85c99e168fe0974562ecb60cee9299617eb
Judy Chen <jc942@duke.edu>
2023-10-18 18:51:01 -0400

    testing

--- TicTacToe/api/app.py
+++ TicTacToe/api/app.py
@@ -113,6 +113,7 @@ except Exception:
 
 if __name__ == '__main__':
     try:
-        app.run(host='10.194.72.227', port=5000, debug=False)
+        # app.run(host='10.194.72.227', port=5000, debug=False)
+        app.run(host='0.0.0.0', port=5000, debug=False)
     except Exception:
         print("FROM APP: failed to run app.run LOL")
