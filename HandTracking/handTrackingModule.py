a5061375c7d1e8530fff59f3336ceeb2510032fd
Johnny <jac188@duke.edu>
2023-10-18 18:26:09 -0400

    created Handtracking folder and hand tracking file

---
+++ HandTracking/handTrackingModule.py
@@ -0,0 +1,107 @@
+import cv2
+import mediapipe as mp
+
+# source https://www.section.io/engineering-education/creating-a-hand-tracking-module/
+
+class handTracker():
+    """
+    A class used to detect and track hands in an image using the Mediapipe library.
+
+    Attributes:
+    mode (bool): Whether to detect hands in static images or in real-time video streams.
+    maxHands (int): Maximum number of hands to detect.
+    detectionCon (float): Minimum confidence value ([0.0, 1.0]) for hand detection to be considered successful.
+    modelComplexity (int): Complexity of the detection model ([0, 2]).
+    trackCon (float): Minimum confidence value ([0.0, 1.0]) for hand tracking to be considered successful.
+    mpHands (mediapipe.solutions.hands): The Mediapipe Hands solution object.
+    hands (mediapipe.solutions.hands.Hands): The Mediapipe Hands object used for detection and tracking.
+    mpDraw (mediapipe.solutions.drawing_utils): The Mediapipe drawing utilities object.
+
+    Methods:
+    handsFinder(image, draw=True): Detects hands in the given image and returns the image with landmarks drawn on it.
+    positionFinder(image, handNo=0, draw=True): Returns a list of landmark positions for the specified hand in the given image.
+    """
+
+    def __init__(self, mode=False, maxHands=1, detectionCon=0.5, modelComplexity=1, trackCon=0.5):
+        """
+        Initializes a new instance of the handTracker class.
+
+        Args:
+        mode (bool): Whether to detect hands in static images or in real-time video streams.
+        maxHands (int): Maximum number of hands to detect.
+        detectionCon (float): Minimum confidence value ([0.0, 1.0]) for hand detection to be considered successful.
+        modelComplexity (int): Complexity of the detection model ([0, 2]).
+        trackCon (float): Minimum confidence value ([0.0, 1.0]) for hand tracking to be considered successful.
+        """
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.modelComplex = modelComplexity
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def handsFinder(self,image,draw=True):
+        """
+        Detects hands in the given image and returns the image with landmarks drawn on it.
+
+        Args:
+        image (numpy.ndarray): The image to detect hands in.
+        draw (bool): Whether to draw landmarks on the image.
+
+        Returns:
+        numpy.ndarray: The image with landmarks drawn on it.
+        """
+        imageRGB = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imageRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+
+                if draw:
+                    self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
+        return image
+
+    def positionFinder(self,image, handNo=0, draw=True):
+        """
+        Returns a list of landmark positions for the specified hand in the given image.
+
+        Args:
+        image (numpy.ndarray): The image to detect hand landmarks in.
+        handNo (int): The index of the hand to detect landmarks for.
+        draw (bool): Whether to draw a circle around the detected landmarks.
+
+        Returns:
+        list: A list of landmark positions for the specified hand.
+        """
+        lmlist = []
+        if self.results.multi_hand_landmarks:
+            Hand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(Hand.landmark):
+                h,w,c = image.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                lmlist.append([id,cx,cy])
+            if draw:
+                cv2.circle(image,(cx,cy), 15 , (255,0,255), cv2.FILLED)
+
+        return lmlist
+    
+
+def main():
+    cap = cv2.VideoCapture(0)
+    tracker = handTracker()
+
+    while True:
+        success,image = cap.read()
+        image = tracker.handsFinder(image)
+        lmList = tracker.positionFinder(image)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cv2.imshow("Video",image)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
