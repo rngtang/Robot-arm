927d65de9ff3654ae27288b5e37a0a6e991250e0
wuchangji <changji.wu@elephantrobotics.com>
2023-10-20 01:28:46 +0800

    led lights

--- PublicApi/commands.py
+++ PublicApi/commands.py
@@ -23,8 +23,39 @@ def hello_world():
 @app.route("/default")
 def default_pos(): 
     controls.send_angles([0, 0, 0, 0, 0, 0], 70)
+    mc.set_color(255, 255, 255) # start white
     time.sleep(1)
     return '''<h1>default </h1>'''
+
+@app.route("/lights") 
+def change_lights(): # for no parameters, go through colors 
+    for count in range(0,3):
+        #From red (255,0,0) to blue (0,0,255)
+        for i in range(0,255):
+            red = 255 - i
+            green = 0
+            blue = i
+            mc.set_color(red,green,blue)
+            time.sleep(0.001)
+
+        #From blue (0,0,255) to green (0,255,0)
+        for i in range(0,255):
+            red = 0
+            green = i
+            blue = 255 - i
+            mc.set_color(red,green,blue)
+            time.sleep(0.001)   
+
+        #From green (0,255,0) to red (255,0,0)
+        for i in range(0,255):
+            red = i
+            green = 255 - i
+            blue = 0
+            mc.set_color(red,green,blue)
+            time.sleep(0.001)   
+
+    return '''<h1>red, blue, green</h1>'''
+
 
 @app.route("/dance")
 def danai_twerk():
