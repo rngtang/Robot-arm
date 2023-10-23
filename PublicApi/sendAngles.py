e3f62159c736893d690c3f992b9d3c2f2badde88
Raul <ren11@duke.edu>
2023-10-23 14:24:29 -0400

    debugging

--- PublicApi/sendAngles.py
+++ PublicApi/sendAngles.py
@@ -6,25 +6,26 @@ s_angles = Blueprint('sendAngles', __name__)
 def sendAngles():
     controls = current_app.config['controls']
     # Checks if the user is sending in JSON format
-    if not request.is_json():
-        return jsonify({"success": False,
-                        "message": "Parameter is not in JSON format"
-        })
+    # if not request.is_json():
+    #     return jsonify({"success": False,
+    #                     "message": "Parameter is not in JSON format"
+    #     })
     
-    # Gets angles and speed from the parameters
-    angles = request.json.get('angles')
-    speed = request.json.get('speed')
+    # # Gets angles and speed from the parameters
+    # angles = request.json.get('angles')
+    # speed = request.json.get('speed')
 
-    # Checks if the parameters are in the correct format and within range
-    if len(angles) != 6 or not speed or speed < 0 or speed > 100:
-        return jsonify({"success": False,
-                        "message": "Invalid parameters"})
+    # # Checks if the parameters are in the correct format and within range
+    # if len(angles) != 6 or not speed or speed < 0 or speed > 100:
+    #     return jsonify({"success": False,
+    #                     "message": "Invalid parameters"})
     
-    # Tries to catch errors
-    try:
-        controls.send_angles(angles, speed)
-        return jsonify({"success": True,
-                        "message": "Angles sent successfully"})
-    except:
-        return jsonify({"success": False,
-                        "message": "Failed to send angles"})
+    # # Tries to catch errors
+    # try:
+    #     controls.send_angles(angles, speed)
+    #     return jsonify({"success": True,
+    #                     "message": "Angles sent successfully"})
+    # except:
+    #     return jsonify({"success": False,
+    #                     "message": "Failed to send angles"})
+    return 'test'
