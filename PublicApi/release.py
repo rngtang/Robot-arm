d70c755a616174218ab9db3c36acd171fb156a50
wuchangji <changji.wu@elephantrobotics.com>
2023-10-24 07:11:42 +0800

    added code

--- PublicApi/release.py
+++ PublicApi/release.py
@@ -8,7 +8,7 @@ def releaseJoints():
     controls = current_app.config['controls']
     try: 
         controls.toggle_servo_released(True)
-        return '''<h1>release </h1>'''
+        return '''<h1>release </h1>''', 200
     except:
-        return '''<h1>Unable to get release joints, try again</h1>'''
+        return '''<h1>Unable to get release joints, try again</h1>''', 400
    
