7bd5a14b2f66e5a3f939d328591412fc130b5b4a
Raul <ren11@duke.edu>
2023-10-23 13:47:50 -0400

    debugging

--- PublicApi/getAngles.py
+++ PublicApi/getAngles.py
@@ -9,8 +9,9 @@ def getAngles():
     # Tries to catch any errors
     try:
         angles = controls.get_angles()
-        return '''<h1>Joint 1: {0}\nJoint 2: {1}\nJoint 3: {2}\nJoint 4: {3}\nJoint 5: {4}\nJoint 6: {5}</h1>'''.format(
-            angles[0], angles[1], angles[2], angles[3],  angles[4], angles[5], angles[6]
-        )
+        # return '''<h1>Joint 1: {0}\nJoint 2: {1}\nJoint 3: {2}\nJoint 4: {3}\nJoint 5: {4}\nJoint 6: {5}</h1>'''.format(
+        #     angles[0], angles[1], angles[2], angles[3],  angles[4], angles[5], angles[6]
+        # )
+        return 'sdf'
     except:
         return '''<h1>Unable to get angles, try again</h1>'''
