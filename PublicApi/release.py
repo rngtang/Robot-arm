f4ebb03dc09d3277c161402ee06560ffe387b013
wuchangji <changji.wu@elephantrobotics.com>
2023-10-24 07:07:46 +0800

    added release route

--- PublicApi/release.py
+++ PublicApi/release.py
@@ -1,17 +1,14 @@
 from flask import Blueprint, current_app
-import time
-import sys
 
 # Creates the blueprint
 release = Blueprint('release', __name__)
 
-# for lights and dance (asynch)
-from pymycobot.mycobot import MyCobot
-mc = MyCobot("/dev/ttyAMA0", 1000000)
-
 @release.route("/")
-def release_joints():
+def releaseJoints():
     controls = current_app.config['controls']
-    controls.toggle_servo_release(True)
-    time.sleep(1)
-    return '''<h1>release </h1>'''
+    try: 
+        controls.toggle_servo_released(True)
+        return '''<h1>release </h1>'''
+    except:
+        return '''<h1>Unable to get release joints, try again</h1>'''
+   
