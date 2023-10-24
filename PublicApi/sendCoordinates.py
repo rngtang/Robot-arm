d9519ffb55d9e3553bf41ad3bc653e0a8dbce21c
Raul <ren11@duke.edu>
2023-10-24 09:28:10 -0400

    test

--- PublicApi/sendCoordinates.py
+++ PublicApi/sendCoordinates.py
@@ -16,9 +16,9 @@ def sendCoordinates():
     coordinates = request.json.get('coords')
     speed = request.json.get('speed')
     mode =  request.json.get('mode')
-    # or not mode or (mode != 0 and mode != 1)
+    
     # Checks if the parameters are in the correct format and within range
-    if len(coordinates) != 6 or not speed or (speed < 0 or speed > 100):
+    if len(coordinates) != 6 or not speed or (speed < 0 or speed > 100) or not mode or (mode != 0 and mode != 1):
         return jsonify({"success": False,
                         "message": "Invalid parameters"}), 400
     
