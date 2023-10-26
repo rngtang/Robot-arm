256e89d07e351082a3548d5cffcc1e93f92bd10b
wuchangji <changji.wu@elephantrobotics.com>
2023-10-27 01:45:57 +0800

    testing without response

--- TicTacToe/ttt.py
+++ TicTacToe/ttt.py
@@ -146,7 +146,7 @@ def updateBoard(board):
 	# try:
 	move = findBestMove(board)
 	url = "http://10.194.72.227:5000/move?pos={pos}".format(pos=str(move[0]) + str(move[1]))
-	response = requests.get(url)
+	# response = requests.get(url)
 	# 	time.sleep(0.1)
 	# 	response.raise_for_status()  # Raise an error for HTTP codes other than 2xx
 	# 	print("FROM TTT: Request successful")
