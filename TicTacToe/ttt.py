381d9c5bb667f7810208cdd66048aed64f08d483
wuchangji <changji.wu@elephantrobotics.com>
2023-10-27 01:57:41 +0800

    testing with threading ???

--- TicTacToe/ttt.py
+++ TicTacToe/ttt.py
@@ -141,16 +141,29 @@ def findBestMove(board):
 	return bestMove
 
 
-# Finds the best move and sends the POST request to the robot
+import threading
+
 def updateBoard(board):
-	# try:
-	move = findBestMove(board)
-	url = "http://10.194.72.227:5000/move?pos={pos}".format(pos=str(move[0]) + str(move[1]))
-	# response = requests.get(url)
-	# 	time.sleep(0.1)
-	# 	response.raise_for_status()  # Raise an error for HTTP codes other than 2xx
-	# 	print("FROM TTT: Request successful")
-	# except requests.exceptions.HTTPError as http_err:
-	# 	print(f'FROM TTT: HTTP error occurred: {http_err}')
-	# except Exception as err:
-	# 	print(f'FROM TTT: An error occurred: {err}')
+    move = findBestMove(board)
+    url = "http://10.194.72.227:5000/move?pos={pos}".format(pos=str(move[0]) + str(move[1]))
+
+    def send_request():
+        requests.get(url)
+
+    thread = threading.Thread(target=send_request)
+    thread.start()
+
+
+# # Finds the best move and sends the POST request to the robot
+# def updateBoard(board):
+# 	try:
+# 		move = findBestMove(board)
+# 		url = "http://10.194.72.227:5000/move?pos={pos}".format(pos=str(move[0]) + str(move[1]))
+# 		response = requests.get(url)
+# 		time.sleep(0.1)
+# 		response.raise_for_status()  # Raise an error for HTTP codes other than 2xx
+# 		print("FROM TTT: Request successful")
+# 	except requests.exceptions.HTTPError as http_err:
+# 		print(f'FROM TTT: HTTP error occurred: {http_err}')
+# 	except Exception as err:
+# 		print(f'FROM TTT: An error occurred: {err}')
