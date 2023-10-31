7104c7836aa127804efa6e6dc7f561047edec1ab
Raul <ren11@duke.edu>
2023-10-31 09:40:45 -0400

    trying to deploy

--- PublicApi/gunicorn_config.py
+++ PublicApi/gunicorn_config.py
@@ -6,7 +6,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
 bind = os.environ.get('GUNICORN_BIND', '10.194.72.227:5000')
 
