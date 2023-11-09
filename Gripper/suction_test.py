4947088282257eeee1d2795251acd9e78f3b9d56
Raul <ren11@duke.edu>
2023-11-09 17:23:52 -0500

    suction test

--- Gripper/suction_test.py
+++ Gripper/suction_test.py
@@ -15,13 +15,13 @@ GPIO.setup(20, GPIO.OUT)
 # Turn on the suction pump
 def pump_on():
      # open suction pump
-    GPIO.output(20，0)
+    GPIO.output(20, 0)
     #GPIO.output(21，0)
 
 # Stop suction pump
 def pump_off():
     # Shut down the suction pump
-    GPIO.output(20，1)
+    GPIO.output(20, 1)
     #GPIO.output(21，1)
 
 pump_off()
