91e1f109322ea0fad1b5fa1e405369b783a4a375
wuchangji <changji.wu@elephantrobotics.com>
2023-11-10 03:37:59 +0800

    grab

--- Gripper/grab.py
+++ Gripper/grab.py
@@ -19,14 +19,15 @@ def grab(mc):
     controls.send_angles([4.21000003815, 1.23000001907, -90.6100006104, -3.16000008583, 96.4100036621, -149.940002441], 60)
     time.sleep(1)
 
+
     # go to grab
+    print("open")
+    mc.set_gripper_state(0, 70)
+    time.sleep(2)
     controls.send_angles([4.21000003815, -58.9700012207, -78.9199981689, 31.9899997711, 86.6600036621, -152.660003662], 60)
     time.sleep(2)
 
     # grab
-    print("open")
-    mc.set_gripper_state(0, 70)
-    time.sleep(2)
     print("close")
     mc.set_gripper_state(1, 70)
     time.sleep(2)
