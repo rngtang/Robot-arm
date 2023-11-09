885c1ea4be540c3747ecf075b60cbb31d6f5c699
Raul <ren11@duke.edu>
2023-11-09 09:27:31 -0500

    debugging

--- runApi.sh
+++ runApi.sh
@@ -1,9 +1,11 @@
 # ./launch_controls.sh
 # ^^ commented out because it potentially takes some time for ./launch_controls.py to execute, just run ./launch_controls.sh manually
 
+# cd PublicApi/
+nohup gunicorn -c gunicorn_config.py main:app --log-level debug &
+# nohup python main.py &
+sleep 5
 cd PublicApi/
-# gunicorn -c gunicorn_config.py main:app --log-level debug 
-nohup python main.py & 
-sleep 3
-cd ..
-./launch_controls.sh
+nohup python main.py
+# cd ..
+# ./launch_controls.sh
