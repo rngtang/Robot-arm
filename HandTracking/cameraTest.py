ce3ae3b5bebf48a5f3e32b51de02fcc521bcc19c
Johnny <jac188@duke.edu>
2023-11-09 12:47:00 -0500

    testing for camera

--- HandTracking/cameraTest.py
+++ HandTracking/cameraTest.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
@@ -9,11 +10,11 @@ else:
     rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
+    # cv2.imshow("preview", frame)
     rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+    # key = cv2.waitKey(20)
+    # if key == 27: # exit on ESC
+    #     break
 
 vc.release()
-cv2.destroyWindow("preview")
+# cv2.destroyWindow("preview")
