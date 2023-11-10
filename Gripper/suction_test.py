ef33f85df31b84163fe2aec7b15ce2bbb125d72b
Raul <ren11@duke.edu>
2023-11-10 09:40:29 -0500

    testing pump

--- Gripper/suction_test.py
+++ Gripper/suction_test.py
@@ -9,20 +9,20 @@ mc = MyCobot(PI_PORT, PI_BAUD)
 # Initialize
 GPIO.setmode(GPIO.BCM)
 # Either pin 20/21 can control the switch of the suction pump. Note: the switch should use the same pin Foot control
-# GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+# GPIO.setup(21, GPIO.OUT)
 
 # Turn on the suction pump
 def pump_on():
     # open suction pump
-    # GPIO.output(20, 0)
-    GPIO.output(21, 0)
+    GPIO.output(20, 0)
+    # GPIO.output(21, 0)
 
 # Stop suction pump
 def pump_off():
     # Shut down the suction pump
-    # GPIO.output(20, 1)
-    GPIO.output(21, 1)
+    GPIO.output(20, 1)
+    # GPIO.output(21, 1)
 
 pump_off()
 time.sleep(3)
