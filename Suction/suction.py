0d83e217df97f593cd4f5f3945e079ea36751d51
Raul <ren11@duke.edu>
2023-11-13 14:09:24 -0500

    test pump

--- Suction/suction.py
+++ Suction/suction.py
@@ -7,7 +7,7 @@ sys.path.append('/home/ubuntu/catkin_ws/src/mycobot_ros/mycobot_280/mycobot_280/
 from controls import Controls
 controls = Controls()
 
-down = [15.98, -59.98, -78.91, 47.45, -1.23, 13.27]
+down = [15.98, -59.98, -78.91, 47.45, -1.23, 0]
 default = [0, 0, 0, 0, 0, 0]
 
 # Sets the what is the numbering of the pins
