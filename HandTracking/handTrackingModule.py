82a3c29046bc6d4e09472b0a9ce0990b5bca725a
Johnny <jac188@duke.edu>
2024-01-26 15:30:47 -0500

    test

--- HandTracking/handTrackingModule.py
+++ HandTracking/handTrackingModule.py
@@ -127,8 +127,8 @@ def main():
             # if lmList[13][1] > 420  and j1 > -325:
             #     j1 = j1 - 2 - (lmList[13][1]/420)
             #     print(j1)
-            j1 = j1 - 0.015*(lmList[13][1]-305)
-
+            if(lmList[13][1] < 230 or lmList[13][1] > 380):
+                j1 = j1 - 0.015*(lmList[13][1]-305)
             # if lmList[13][3] < -0.07 and j2 < 40:
             #     j2 = j2 + 2
             #     j3 = j3 - 2
