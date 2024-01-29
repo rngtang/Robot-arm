2d69a54da17b3bd826b76b5e27120467aa1ab06c
Johnny <jac188@duke.edu>
2024-01-29 13:16:40 -0500

    test

--- HandTracking/handTrackingModule.py
+++ HandTracking/handTrackingModule.py
@@ -121,14 +121,12 @@ def main():
         lmList = tracker.positionFinder(image)
         # using index 13 data point near center of hand
         if len(lmList) != 0:
-            # if lmList[13][1] < 190 and j1 < 325:
-            #     j1 = j1 + 2 + (lmList[13][1]/190)
-            #     print(j1)
-            # if lmList[13][1] > 420  and j1 > -325:
-            #     j1 = j1 - 2 - (lmList[13][1]/420)
-            #     print(j1)
-            if(lmList[13][1] < 230 or lmList[13][1] > 380):
-                j1 = j1 - 0.01*(lmList[13][1]-305)
+            if lmList[13][1] < 190 and j1 < 325:
+                j1 = j1 + 2 + (lmList[13][1]/190)
+            if lmList[13][1] > 420  and j1 > -325:
+                j1 = j1 - 2 - (lmList[13][1]/420)
+            # if(lmList[13][1] < 230 or lmList[13][1] > 380):
+            #     j1 = j1 - 0.01*(lmList[13][1]-305)
             if lmList[13][3] < -0.07 and j2 < 40:
                 j2 = j2 + 2
                 j3 = j3 - 2
