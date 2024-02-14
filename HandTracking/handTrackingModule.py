ab63d58530e8f877d2b1ff63f862c02b045665ea
project_35480_bot_f42036c14775653a21907a0cddd40e37 <project_35480_bot_f42036c14775653a21907a0cddd40e37@noreply.gitlab.oit.duke.edu>
2024-02-15 04:05:39 +0800

    parameter tweaking

--- HandTracking/handTrackingModule.py
+++ HandTracking/handTrackingModule.py
@@ -8,6 +8,9 @@ from pymycobot import PI_PORT, PI_BAUD
 from pymycobot.mypalletizer import MyPalletizer
 from pymycobot.genre import Coord
 
+import numpy as np
+import threading
+
 # import sys
 # sys.path.append('/home/ubuntu/catkin_ws/src/mycobot_ros/mycobot_280/mycobot_280/scripts')
 # from pymycobot.mycobot import MyCobot
@@ -103,42 +106,80 @@ class handTracker():
                 cz = landmark.z # add z-coordinate
                 lmList.append([id, cx, cy, cz]) # append x, y, and z coordinates to list
         return lmList
+
+class CameraFlangeController:
+    def __init__(self):
+        self.mc = MyCobot("/dev/ttyAMA0", 1000000)
+        self.mc.send_angles([0, 0, 0, 0, 0, -135], 40)
+        self.cap = cv2.VideoCapture(0)
+        #lower res means faster tracking
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)  # Set width
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)  # Set height
+        self.tracker = handTracker()
+        self.j1, self.j4 = 0, 0
+        self.running = True
+
+    def start(self):
+        threading.Thread(target=self.control_loop, daemon=True).start()
+
+    def stop(self):
+        self.running = False
+
+    def control_loop(self):
+        while self.running:
+            success, image = self.cap.read()
+            if not success:
+                continue
+            
+            image = self.tracker.handsFinder(image)
+            lmList = self.tracker.positionFinder(image)
+            
+            if len(lmList) >= 12:
+                x, y, z = lmList[13][1], lmList[13][2], lmList[13][3]
+                if x < 155 or x > 155:
+                    self.j1 -= 0.01 * (x - 155)
+                if y < 107 or y > 107:
+                    self.j4 -= 0.01 * (y - 107)
+                # Send joint angles to MyCobot
+                self.mc.send_angles([self.j1, 0, 0, self.j4, 0, -135], 80)
+            
+            cv2.imshow("Video", image)
+            cv2.waitKey(1)
     
 def main():
     mc = MyCobot("/dev/ttyAMA0", 1000000)
-    mc.send_angles([0, 0, 0, 0, 0, 45], 30)
+    mc.send_angles([0, 0, 0, 0, 0, -135], 40)
     cap = cv2.VideoCapture(0)
     tracker = handTracker()
-    j1 = 0
-    j2 = 0
-    j3 = 0
-    j4 = 0
+    j1, j2, j3, j4 = 0, 0, 0, 0
     while True:
         success,image = cap.read()
         image = tracker.handsFinder(image)
         lmList = tracker.positionFinder(image)
-        # using index 13 data point near center of hand
+        #using index 13 for point 13 near center of hand. This index's location on hand may vary if point 13 is not at index 13 in lmList.       
         if len(lmList) != 0:
-            # if lmList[13][1] < 190 and j1 < 325:
-            #     j1 = j1 + 2
-            # if lmList[13][1] > 420  and j1 > -325:
-            #     j1 = j1 - 2
-            if(lmList[13][1] < 230 or lmList[13][1] > 380):
-                j1 = j1 - 0.01*(lmList[13][1]-305)
-            if lmList[13][3] < -0.07 and j2 < 40:
-                j2 = j2 + 2
-                j3 = j3 - 2
-            if lmList[13][3] > -0.03 and j2 > -60:
-                j2 = j2 - 2
-                j3 = j3 + 2
-            if lmList[13][2] < 200 and j4 < 120:
-                j4 = j4 + 1
-            if lmList[13][2] > 360 and j4 > -60:
-                j4 = j4 - 1
-            mc.send_angles([j1, j2, j3, j4, 0, 45], 80)
-            # print("------------", lmList, "------------")
+            x, y, z = lmList[13][1], lmList[13][2], lmList[13][3]
+            if  x < 290 or x > 310:
+                j1 = j1 - 0.007*(x - 300)
+            #if z < -0.09 and j2 < 90:
+                #j2 = j2 - 40*(z + 0.055)
+                #j3 = j3 + 40*(z + 0.055)
+            #if z > -0.02 and j2 > -90:
+                #j2 = j2 - 40*(z + 0.055)
+                #j3 = j3 + 40*(z + 0.055)
+            if y < 205 or y > 225:
+                j4 = j4 - 0.007*(y - 215)
+            mc.send_angles([j1, j2, j3, j4, 0, -135], 100)
+            #print("------------", lmList, "------------")
         cv2.imshow("Video",image)
         cv2.waitKey(1)
 
 if __name__ == "__main__":
-    main()
+    #tracking without concurrency and additional imporvements
+    #main()
+
+    #tracking with concurrency and additional improvements
+    controller = CameraFlangeController()
+    controller.start()
+    input("Press Enter to stop...")
+    controller.stop()
