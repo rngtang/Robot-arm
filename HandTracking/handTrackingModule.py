43cf798cab64d5c814f7fb40b7085b60af2aa2bc
project_35480_bot_f42036c14775653a21907a0cddd40e37 <project_35480_bot_f42036c14775653a21907a0cddd40e37@noreply.gitlab.oit.duke.edu>
2024-03-09 03:10:39 +0800

    fixed lag

--- HandTracking/handTrackingModule.py
+++ HandTracking/handTrackingModule.py
@@ -216,7 +216,7 @@ class CameraFlangeController:
             gesture = result.gestures[0][0].category_name
             if gesture == "Thumb_Up" and self.j2 > -130:
                 if self.prevGesture == "Thumb_Up":
-                    if self.multiplier < 7:
+                    if self.multiplier < 5:
                         # print("test1")
                         self.multiplier += 1
                 else:
@@ -226,7 +226,7 @@ class CameraFlangeController:
                 self.prevGesture = "Thumb_Up"
             elif gesture == "Pointing_Up" and self.j2 < 130:
                 if self.prevGesture == "Pointing_Up":
-                    if self.multiplier < 7:
+                    if self.multiplier < 5:
                         # print("test2")
                         self.multiplier += 1
                 else:
@@ -270,24 +270,32 @@ class CameraFlangeController:
                 j2_ema_new = alpha * self.j2 + (1 - alpha) * j2_ema
                 j2_delta = j2_ema_new - j2_ema #track wheather going forward or backward
                 #print(self.prevGesture)
-                print("----")
-                print(j2_ema)
-                print(j2_delta)
-                print(j2_ema_new)
-                print("----")
+                # print("----")
+                # print(j2_ema)
+                # print(j2_delta)
+                # print(j2_ema_new)
+                # print("----")
                 #should try and fix the lag when switching from gesture to gesture
                 if (self.prevGesture == "Thumb_Up" and j2_delta < 0 and j2_ema_new > -90):
                     j2_ema = j2_ema_new #update join movement
                     j3_ema = alpha * self.j3 + (1 - alpha) * j3_ema
+                    # print(self.multiplier)
                 elif (self.prevGesture == "Thumb_Up" and j2_delta > 0 and j2_ema - 2 > -90):
+                    self.j2 = j2_ema
                     j2_ema -= 1 #update join movement
+                    self.j3 = j3_ema
                     j3_ema += 1
+                    # print("test1")
                 elif (self.prevGesture == "Pointing_Up" and j2_delta > 0 and j2_ema_new < 90):
                     j2_ema = j2_ema_new #update join movement
                     j3_ema = alpha * self.j3 + (1 - alpha) * j3_ema
+                    # print(self.multiplier)
                 elif (self.prevGesture == "Pointing_Up" and j2_delta < 0 and j2_ema + 2 < 90):
+                    self.j2 = j2_ema
                     j2_ema += 1 #update join movement
+                    self.j3 = j3_ema
                     j3_ema -= 1
+                    # print("test2")
                 # Send joint angles to MyCobot
                 self.mc.send_angles([self.j1, j2_ema, j3_ema, self.j4, 0, -135], 100)      
 
