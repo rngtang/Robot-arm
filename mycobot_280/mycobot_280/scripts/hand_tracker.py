a48809f0c94196a0a6c4bccbe0d77667e8579ff7
project_35480_bot_f42036c14775653a21907a0cddd40e37 <project_35480_bot_f42036c14775653a21907a0cddd40e37@noreply.gitlab.oit.duke.edu>
2024-03-30 02:23:04 +0800

    experiments to smooth out robot movement

--- mycobot_280/mycobot_280/scripts/hand_tracker.py
+++ mycobot_280/mycobot_280/scripts/hand_tracker.py
@@ -11,8 +11,8 @@ class HandTracker:
     CENTER_X = 160
     CENTER_Y = 120
 
-    DISTANCE_COEFFICIENT = 0.005
-    SPEED_COEFFICIENT = -0.007
+    DISTANCE_COEFFICIENT = 0.015
+    SPEED_COEFFICIENT = -0.020
 
     def __init__(self) -> None:
         rospy.init_node("hand_tracker")
@@ -34,7 +34,7 @@ class HandTracker:
         if new_y != self.CENTER_Y:
             self.j4 += self.DISTANCE_COEFFICIENT * (self.CENTER_Y - new_y) + self.SPEED_COEFFICIENT * (new_y - self.y)
 
-        self.mc.send_angles([self.j1, self.j2, self.j3, self.j4, 0, -135], 100)
+        self.mc.send_angles([self.j1, self.j2, self.j3, self.j4, 0, -135], 0)
         self.x, self.y = data.data
 
 
