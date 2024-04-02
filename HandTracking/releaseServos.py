57fa8c5c4b2681c202ca9de03f7a156d69727ba7
project_35480_bot_f42036c14775653a21907a0cddd40e37 <project_35480_bot_f42036c14775653a21907a0cddd40e37@noreply.gitlab.oit.duke.edu>
2024-04-03 05:37:21 +0800

    edits

--- HandTracking/releaseServos.py
+++ HandTracking/releaseServos.py
@@ -3,6 +3,23 @@ from pymycobot.genre import Angle
 from pymycobot import PI_PORT, PI_BAUD
 from pymycobot.mypalletizer import MyPalletizer
 from pymycobot.genre import Coord
+import time
+import math
 
 mc = MyCobot("/dev/ttyAMA0", 1000000)
 mc.release_all_servos()
+mc.send_angles([0, 0, 0, 0, 0, 0], 40)
+time.sleep(10)
+coords = mc.get_coords()
+# time.sleep(8)
+print(math.sqrt(coords[0]**2 + coords[1]**2))
+
+# mc.send_angles([0, -90, 0, 0, 0, 0], 40)
+# time.sleep(12)
+# coords = mc.get_coords()
+# time.sleep(8)
+# print(math.sqrt(coords[0]**2 + coords[1]**2) - 90)
+# while True:
+#     coords = mc.get_coords()
+#     if len(coords) > 0:
+#         print(coords)
