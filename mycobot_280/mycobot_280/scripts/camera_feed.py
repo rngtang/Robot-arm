b55f71ade9803826efe21ba7ec9040f105aff536
viethungle-vt1401 <brandon.viethungRI2001@gmail.com>
2024-04-23 18:31:19 -0400

    comments on camera_feed.py

--- mycobot_280/mycobot_280/scripts/camera_feed.py
+++ mycobot_280/mycobot_280/scripts/camera_feed.py
@@ -9,9 +9,25 @@ from sensor_msgs.msg import CompressedImage
 
 
 class CameraFeedPublisher:
+    """
+    A class used to launch a ROS (Robot Operating System) node that captures video feed from a camera, 
+    compresses it, and publishes it as a ROS topic.
+
+    Attributes:
+        cap (cv2.VideoCapture): Video capture object from OpenCV for accessing camera feed.
+        loop_rate (rospy.Rate): Rate object from rospy to control the loop rate of the node. Set default to 30 Hz.
+        _cv_bridge (CvBridge): CvBridge object for converting between OpenCV images and ROS image messages.
+        publisher (rospy.Publisher): ROS publisher object for publishing compressed image messages.
+
+    Methods:
+        run(): Method to start capturing frames from the camera, converting them to ROS image messages,
+               and publishing them continuously in a loop at the frequency of loop_rate.
+    """
     def __init__(self) -> None:
+        # Initialize ROS node with the name "camera_feed_publisher"
         rospy.init_node("camera_feed_publisher")
 
+        # Initialize video capture object for default camera (channel 0)
         self.cap = cv2.VideoCapture(0)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)  # Set width
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)  # Set height
@@ -21,16 +37,17 @@ class CameraFeedPublisher:
 
         # Set publisher rate (framerate) to custom framerate
         self.loop_rate = rospy.Rate(self.cap.get(cv2.CAP_PROP_FPS))
-        # self.loop_rate = rospy.Rate(10)
 
+        # Create CvBridge object for image conversion
         self._cv_bridge = CvBridge()
 
+        # Create ROS publisher for compressed image topic
         self.publisher = rospy.Publisher("camera/rgb/compressed", CompressedImage, queue_size=10)
 
-        self.running = True
-
-    def start(self):
-        while not rospy.is_shutdown() and self.running:
+    def run(self):
+        # Loop until ROS shutdown or node is stopped
+        while not rospy.is_shutdown():
+            # Read frame from camera
             success, image = self.cap.read()
             if not success:
                 print("Camera did not receive image feed.")
@@ -40,12 +57,9 @@ class CameraFeedPublisher:
             image_msg = self._cv_bridge.cv2_to_compressed_imgmsg(image)
             self.publisher.publish(image_msg)
 
+            # Sleep to maintain loop rate
             self.loop_rate.sleep()
-
-    def stop(self):
-        self.running = False
 
 
 if __name__ == "__main__":
-    camera = CameraFeedPublisher()
-    camera.start()
+    CameraFeedPublisher().run()
